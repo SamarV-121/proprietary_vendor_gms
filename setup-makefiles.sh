869d157b3fab97ebc7fbfc6676a5eaf1e792fb15
SamarV-121 <samarvispute121@pm.me>
2022-08-19 12:50:39 +0530

    gms: Setup extraction scripts
    Change-Id: I1d5025ff0b049c2efcc99f0901b60ab1965160c2

---
+++ setup-makefiles.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=common
+VENDOR=gms
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" true
+
+# Warning headers and guards
+write_headers "arm64" "TARGET_ARCH"
+sed -i 's/vendor\/gms\//vendor\/gms\/common/' "${PRODUCTMK}"
+
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+# HAX
+awk '/PrebuiltGmsCoreSc/ {print NR-2 "," NR+7 "d"}' "${ANDROIDBP}" | sed -f - -i "${ANDROIDBP}"
+cat <<EOF >>"${ANDROIDMK}"
+\$(PRODUCT_OUT)/obj/GMS/PrebuiltGmsCoreSc.apk: \$(MINIGZIP)
+$(printf '\t')@rm -rf \$(dir \$@)
+$(printf '\t')@mkdir -p \$(dir \$@)
+$(printf '\t')@\$(MINIGZIP) -c -d vendor/gms/common/proprietary/product/priv-app/PrebuiltGmsCore/PrebuiltGmsCoreSc.apk.gz > \$@
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := PrebuiltGmsCoreSc
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_ENFORCE_USES_LIBRARIES := false
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := ../../../\$(PRODUCT_OUT)/obj/GMS/PrebuiltGmsCoreSc.apk
+include \$(BUILD_PREBUILT)
+
+EOF
+gzip -f "${ANDROID_ROOT}/${OUTDIR}/proprietary/product/priv-app/PrebuiltGmsCore/PrebuiltGmsCoreSc.apk"
+
+# Finish
+write_footers
